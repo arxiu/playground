bbc078eec7a1276bf6d72ca7eb9f6021fc207c9c
Xavi Vives <xavings@gmail.com>
2018-04-09 23:26:22 +0200

    Generating hitags

--- src/app.js
+++ src/app.js
@@ -2,7 +2,41 @@
 const ipfs = require('./ipfsNode')
 const format =  { format: 'dag-cbor', hashAlg: 'sha2-256' }
 
+let baseProps = {
+    canRead:[],
+    canWrite:[],
+    hitags:[],
+    history:[]
+}
 
+let getBaseProps=()=>
+{
+    return JSON.parse(JSON.stringify(baseProps));
+}
+
+let addChild=(parent, child)=>
+{
+    if(!parent.hitag)
+        parent.hitag =  getBaseProps()
+
+    parent.hitag.hitags.push(child)
+}
+
+let newHitag = (name)=>
+{
+    return {
+        name: name,
+        hitag: getBaseProps()
+    }
+}
+
+let resolveHitag=(cid, hitagPath, key)=>
+{
+    ipfs.dag.get(cid, 'hitag', (e,r)=>{
+        console.log(r)
+    })
+    hitagPath.split('/')
+}
 
 let getData=(error, data)=>
 {
@@ -14,27 +48,23 @@ let onNewCid=(error, cid)=>
     if(error)
         console.error(error)
 
-        console.log(cid.toBaseEncodedString('base58btc'))
+    console.log('Added :',cid.toBaseEncodedString('base58btc'))
 
-        ipfs.dag.get(cid, '', getData)
-        
+    resolveHitag(cid,'Bunga','0x123')
 }
 
 let init=()=>
 {
-    console.log('lets do stuff')
+    let h1 = newHitag('Unga')
+    let h2 = newHitag('Bunga')
+    let h3 = newHitag('Xunga')
 
-    let hitag = {
-        name:'Unga bunga',
-        arxiu:{
-            history:[],
-            canRead :[],
-            canWrite : [],
-            hitags :[]
-        }
-    }
+    addChild(h2, h3)
+    addChild(h1, h2)
 
-    ipfs.dag.put(hitag,format, onNewCid)
+    //console.log(JSON.stringify(h1))
+
+    ipfs.dag.put(h1, format, onNewCid)
 }
 
 ipfs.on('start', init)
